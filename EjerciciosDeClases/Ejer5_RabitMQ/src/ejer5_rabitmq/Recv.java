381da69545a1df7d5e1d91cbb068660827e1247a
Franz Reinaldo Gonzales <gonzales.suyo.franzreinaldo@usfx.bo>
2024-09-12 23:46:00 -0400

    Tarea 5 en clases

--- EjerciciosDeClases/Ejer5_RabitMQ/src/ejer5_rabitmq/Recv.java
+++ EjerciciosDeClases/Ejer5_RabitMQ/src/ejer5_rabitmq/Recv.java
@@ -1,13 +1,70 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package ejer5_rabitmq;
 
-/**
- *
- * @author Franz Gonzales
- */
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+import java.nio.charset.StandardCharsets;
+
 public class Recv {
+
+    private final static String QUEUE_NAME = "operaciones";
+
+    public static void main(String[] argv) throws Exception {
+        
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        factory.setUsername("franz");
+        factory.setPassword("gonzales");
+        
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        System.out.println(" [*] Esperando mensajes. Para salir, presiona CTRL+C");
+
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String operacion = new String(delivery.getBody(), StandardCharsets.UTF_8);
+            
+            String resultado = resolverOperaciones(operacion);
+            System.out.println(" [x] Received '" + resultado + "'");
+        };
+        
+        channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> { });
+    }
     
+    private static String resolverOperaciones(String operaciones){
+        String[] valores = operaciones.split(" ");
+        
+        int numero_veces = Integer.parseInt(valores[1]);
+        int numero1 = Integer.parseInt(valores[1]);
+        char operacion = valores[2].charAt(0);
+        int numero2 = Integer.parseInt(valores[3]);
+        
+        double resultado;
+        
+        switch(operacion){
+            case '+':
+                resultado = numero1 + numero2;
+                break;
+            case '-':
+                resultado = numero1 - numero2;
+                break;
+            case '*':
+                resultado = numero1 * numero2;
+                break;
+            case '/':
+                if(numero2 != 0){
+                    double division = (double) numero1 / numero2;
+                    resultado = Math.round(division * 100.0) / 100.0;
+                }else{
+                    return operacion + "division por sero";
+                }
+                break;
+            default:
+                return "Operacion ivalidada";
+        }
+        
+        return "Ope: " + numero_veces + "=> " + numero1 + " " + operacion + " " + numero2 + " = " + resultado; 
+    }
 }
--- EjerciciosDeClases/Ejer5_RabitMQ/src/ejer5_rabitmq/Send.java
+++ EjerciciosDeClases/Ejer5_RabitMQ/src/ejer5_rabitmq/Send.java
@@ -1,13 +1,49 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package ejer5_rabitmq;
 
-/**
- *
- * @author Franz Gonzales
- */
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Random;
+
 public class Send {
+
+    private final static String QUEUE_NAME = "operaciones";
+
+    public static void main(String[] argv) throws Exception {
+        
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+//        Datos del usuario
+        factory.setUsername("franz");
+        factory.setPassword("gonzales");
+        
+        try (Connection connection = factory.newConnection();
+            Channel channel = connection.createChannel()) {
+            
+            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+            
+            for(int i = 1; i <= 500; i++){
+                
+                String operacion = generarOperacion(i);
+                channel.basicPublish("", QUEUE_NAME, null, operacion.getBytes(StandardCharsets.UTF_8));
+                System.out.println(" [x] Sent '" + operacion + "'");
+            }
+        }
+    }
     
+    
+    private static String generarOperacion(int num){
+        Random random = new Random();
+        char[] operaciones = {'+', '-', '*', '/'};
+        
+        int numero1 = random.nextInt(100) + 1;
+        int numero2 = random.nextInt(100) + 1;
+        int posicion = random.nextInt(operaciones.length);
+        char operacion = operaciones[posicion];
+        
+        return num + " " + numero1 + " " + operacion + " " + numero2;
+    }
 }
